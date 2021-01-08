7e2c42cd2a185d18f8c8278b332a0d4180174d98
AngelValAngelov <64867029+AngelValAngelov@users.noreply.github.com>
2021-01-08 02:21:02 +0200

    Nested Loops - Lab

---
+++ Nested Loops - Lab/01. Clock.js
@@ -0,0 +1,9 @@
+function clock(){
+    for(i = 0; i < 24; i++){
+        for(j = 0; j < 60; j++){
+            console.log(`${i}:${j}`);
+        }
+    }       
+}
+
+clock()
---
+++ Nested Loops - Lab/02. Multiplication Table.js
@@ -0,0 +1,9 @@
+function table(){
+    for(i = 1; i < 11; i++){
+        for(j = 1; j < 11; j++){
+            console.log(`${i} * ${j} = ${i * j}`);
+        }
+    }
+}
+
+table()
